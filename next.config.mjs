b4f136f8c6bc4872e40947754655d94df72352b5
SeveTo <seveto@o2.pl>
2026-03-19 09:25:08 +0100

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // To jest kluczowe dla GitHub Pages
+  basePath: '/el-app', // Tu wpisujesz nazwę swojego repozytorium
+  images: {
+    unoptimized: true, // GitHub Pages to serwer statyczny, Next.js Image Optimization tu nie zadziała
+  },
+};
 
 export default nextConfig;
