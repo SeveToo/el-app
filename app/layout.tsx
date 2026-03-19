38eb51f14ef4202d7721fef7315cb25ec7cde52e
SeveTo <seveto@o2.pl>
2026-03-19 11:36:16 +0100

    fix: poprawa działania position: sticky na mobile przez zmianę h-screen na min-h-screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
         )}>
         <Providers
           themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
-          <div className="relative flex flex-col h-screen">
+          <div className="relative flex flex-col min-h-screen">
             <Navbar />
             <main className="container mx-auto max-w-7xl pt-3 px-3 md:pt-8 md:px-6 flex-grow">
               {children}
